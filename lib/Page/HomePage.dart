f6cb36fed7e88bca0d6c857f14ced4cfbc0efe60
Kitti_pop <jokkerlolyer20@gmail.com>
2020-02-09 22:09:17 +0700

    change column->Stack

--- lib/Page/HomePage.dart
+++ lib/Page/HomePage.dart
@@ -12,88 +12,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     Size a = MediaQuery.of(context).size;
-    return Material(
-      child: Column(
+    return Scaffold(
+      body: Stack(
         children: <Widget>[
-          Container(
-            color: Colors.black,
-            width: a.width,
-            height: a.width / 5,
-            padding: EdgeInsets.only(
-              right: a.width / 20,
-              left: a.width / 20,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                    height: a.width / 5,
-                    alignment: Alignment.center,
-                    child: Text("SCRAP.",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: a.width / 15,
-                        ))),
-                Container(
-                    height: a.width / 5,
-                    alignment: Alignment.center,
-                    child: InkWell(
-                      child: Container(
-                        width: a.width / 10,
-                        height: a.width / 10,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(a.width),
-                          color: Colors.white,
-                        ),
-                        child: Icon(Icons.person,
-                            color: Colors.black, size: a.width / 15),
-                      ),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Profile()));
-                      },
-                    ))
-              ],
-            ),
+          Center(
+            child: Text("data"),
           ),
-          Container(
-            padding: EdgeInsets.only(bottom: a.width / 10),
-            alignment: Alignment.bottomCenter,
-            width: a.width,
-            height: a.height / 1.119,
-            color: Colors.black87,
+          Positioned(
+            bottom: 0,
             child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Container(
-                    width: a.width / 7,
-                    height: a.width / 7,
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.white,
-                            blurRadius: 10.0,
-                            spreadRadius: 0.0,
-                            offset: Offset(
-                              0.0,
-                              3.0,
-                            ),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(a.width),
-                        color: Colors.white),
-                    child: Icon(
-                      Icons.person_add,
-                      color: Colors.black,
-                      size: a.width / 15,
-                    ),
-                  ),
-                  InkWell(
-                    child: Container(
-                      width: a.width / 5,
-                      height: a.width / 5,
+              padding: EdgeInsets.only(bottom: a.width / 10),
+              alignment: Alignment.bottomCenter,
+              width: a.width,
+              height: a.height / 1.119,
+              color: Colors.black87,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Container(
+                      width: a.width / 7,
+                      height: a.width / 7,
                       decoration: BoxDecoration(
                           boxShadow: [
                             BoxShadow(
@@ -108,48 +47,122 @@ class _HomePageState extends State<HomePage> {
                           ],
                           borderRadius: BorderRadius.circular(a.width),
                           color: Colors.white),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Icon(
-                            Icons.create,
-                            size: a.width / 10,
-                            color: Colors.black,
-                          ),
-                        ],
+                      child: Icon(
+                        Icons.person_add,
+                        color: Colors.black,
+                        size: a.width / 15,
                       ),
                     ),
-                    onTap: () {
-                      dialog();
-                    },
-                  ),
-                  Container(
-                    width: a.width / 7,
-                    height: a.width / 7,
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.white,
-                            blurRadius: 10.0,
-                            spreadRadius: 0.0,
-                            offset: Offset(
-                              0.0,
-                              3.0,
+                    InkWell(
+                      child: Container(
+                        width: a.width / 5,
+                        height: a.width / 5,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.white,
+                                blurRadius: 10.0,
+                                spreadRadius: 0.0,
+                                offset: Offset(
+                                  0.0,
+                                  3.0,
+                                ),
+                              )
+                            ],
+                            borderRadius: BorderRadius.circular(a.width),
+                            color: Colors.white),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.create,
+                              size: a.width / 10,
+                              color: Colors.black,
                             ),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(a.width),
-                        color: Colors.blueAccent),
-                    child: Icon(
-                      Icons.refresh,
-                      color: Colors.black,
-                      size: a.width / 15,
+                          ],
+                        ),
+                      ),
+                      onTap: () {
+                        dialog();
+                      },
                     ),
-                  ),
+                    Container(
+                      width: a.width / 7,
+                      height: a.width / 7,
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.white,
+                              blurRadius: 10.0,
+                              spreadRadius: 0.0,
+                              offset: Offset(
+                                0.0,
+                                3.0,
+                              ),
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(a.width),
+                          color: Colors.blueAccent),
+                      child: Icon(
+                        Icons.refresh,
+                        color: Colors.black,
+                        size: a.width / 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Container(
+              color: Colors.black,
+              width: a.width,
+              height: a.width / 4,
+              padding: EdgeInsets.only(
+                top: a.height / 36,
+                right: a.width / 20,
+                left: a.width / 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                      height: a.width / 5,
+                      alignment: Alignment.center,
+                      child: Text("SCRAP.",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: a.width / 15,
+                          ))),
+                  Container(
+                      height: a.width / 5,
+                      alignment: Alignment.center,
+                      child: InkWell(
+                        child: Container(
+                          width: a.width / 10,
+                          height: a.width / 10,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(a.width),
+                            color: Colors.white,
+                          ),
+                          child: Icon(Icons.person,
+                              color: Colors.black, size: a.width / 15),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Profile()));
+                        },
+                      ))
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
@@ -279,8 +292,9 @@ class _HomePageState extends State<HomePage> {
                         margin: EdgeInsets.only(top: a.width / 20),
                         width: a.width / 1.2,
                         height: a.width / 1,
-                        decoration: BoxDecoration(color: Colors.green,borderRadius: BorderRadius.circular(a.width/10)),
-                        
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(a.width / 10)),
                       ),
                       Container(
                           margin: EdgeInsets.only(top: a.width / 10),
