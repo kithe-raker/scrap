bd2f7f9098cc087b122c740ab20b94ec40a275f5
Mosthunter <phuminsathipchan@gmail.com>
2020-02-10 21:41:14 +0700

    แก้ dialog

--- lib/Page/HomePage.dart
+++ lib/Page/HomePage.dart
@@ -329,6 +329,7 @@ class _HomePageState extends State<HomePage> {
                   StatefulBuilder(builder: (context, StateSetter setState) {
                 Size a = MediaQuery.of(context).size;
                 return Container(
+                 
                   width: a.height,
                   height: a.height / 1.3,
                   child: ListView(
@@ -351,6 +352,7 @@ class _HomePageState extends State<HomePage> {
                                           width: a.width / 15,
                                           height: a.width / 15,
                                           decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(a.width/50),
                                               border: Border.all(
                                                   color: Colors.white)),
                                         ),
@@ -384,7 +386,7 @@ class _HomePageState extends State<HomePage> {
                               height: a.height / 2,
                               child: Stack(
                                 children: <Widget>[
-                                  Container(
+                                  Container( 
                                     child: Image.asset(
                                       'assets/paper-readed.png',
                                       width: a.width / 1,
@@ -418,26 +420,32 @@ class _HomePageState extends State<HomePage> {
                                     width: a.width,
                                     height: a.height,
                                     alignment: Alignment.center,
-                                    child: TextFormField(
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(fontSize: a.width / 15),
-                                      decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: 'เขียนข้อความบางอย่าง',
-                                        hintStyle: TextStyle(
-                                          fontSize: a.width / 15,
-                                          color: Colors.grey,
+                                    child: SizedBox(
+                                      width: a.width/3,
+                                      child: TextFormField(
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(fontSize: a.width / 15),
+                                        maxLines: null,
+                                        decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                          hintText: 'เขียนข้อความบางอย่าง',
+                                          
+                                          hintStyle: TextStyle(
+
+                                            fontSize: a.width / 25,
+                                            color: Colors.grey,
+                                          ),
                                         ),
+                                        validator: (val) {
+                                          return val.trim() == null ||
+                                                  val.trim() == ""
+                                              ? 'กรอกค่ะ'
+                                              : null;
+                                        },
+                                        onChanged: (val) {
+                                          text = val;
+                                        },
                                       ),
-                                      validator: (val) {
-                                        return val.trim() == null ||
-                                                val.trim() == ""
-                                            ? 'กรอกค่ะ'
-                                            : null;
-                                      },
-                                      onChanged: (val) {
-                                        text = val;
-                                      },
                                     ),
                                   )
                                   //)
@@ -452,10 +460,10 @@ class _HomePageState extends State<HomePage> {
                                 children: <Widget>[
                                   InkWell(
                                     child: Container(
-                                        width: a.width / 4,
-                                        height: a.width / 7,
+                                        width: a.width / 4.5,
+                                        height: a.width / 8,
                                         decoration: BoxDecoration(
-                                            color: Colors.grey,
+                                            color: Colors.white,
                                             borderRadius:
                                                 BorderRadius.circular(a.width)),
                                         alignment: Alignment.center,
@@ -482,10 +490,10 @@ class _HomePageState extends State<HomePage> {
                                   ),
                                   InkWell(
                                     child: Container(
-                                      width: a.width / 4,
-                                      height: a.width / 7,
+                                      width: a.width / 4.5,
+                                      height: a.width / 8,
                                       decoration: BoxDecoration(
-                                          color: Colors.grey,
+                                          color: Colors.white,
                                           borderRadius:
                                               BorderRadius.circular(a.width)),
                                       alignment: Alignment.center,
@@ -552,7 +560,7 @@ class _HomePageState extends State<HomePage> {
                           Container(
                             margin: EdgeInsets.only(top: a.width / 20),
                             width: a.width / 1.1,
-                            height: a.height/1.7,
+                            height: a.height / 1.7,
                             decoration: BoxDecoration(
                                 color: Colors.grey,
                                 borderRadius:
@@ -568,7 +576,8 @@ class _HomePageState extends State<HomePage> {
                                   ),
                                   width: a.width / 1.7,
                                   height: a.width / 7,
-                                  child: Container(margin: EdgeInsets.only(left: a.width/20),
+                                  child: Container(
+                                    margin: EdgeInsets.only(left: a.width / 20),
                                     child: TextField(
                                       decoration: InputDecoration(
                                         border: InputBorder.none,
@@ -588,10 +597,10 @@ class _HomePageState extends State<HomePage> {
                               margin: EdgeInsets.only(top: a.width / 10),
                               child: InkWell(
                                 child: Container(
-                                  width: a.width / 3.5,
-                                  height: a.width / 6.5,
+                                  width: a.width / 4.5,
+                                  height: a.width / 8,
                                   decoration: BoxDecoration(
-                                      color: Colors.grey,
+                                      color: Colors.white,
                                       borderRadius:
                                           BorderRadius.circular(a.width)),
                                   alignment: Alignment.center,
