4a75ea4d5b1e4f14782c3b66fd4d25a2682d477c
Mosthunter <phuminsathipchan@gmail.com>
2020-02-17 19:41:15 +0700

    1

--- lib/Page/addPlayer.dart
+++ lib/Page/addPlayer.dart
@@ -14,31 +14,53 @@ class _AddPlayerState extends State<AddPlayer> {
         width: a.width,
         height: a.height,
         color: Colors.black,
-        child: Column(children: <Widget>[
-          Row(
-                              children: <Widget>[
-                                InkWell(
-                                  //back btn
-                                  child: Container(
-                                    width: a.width / 7,
-                                    height: a.width / 10,
-                                    decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(a.width),
-                                        color: Colors.white),
-                                    child: Icon(Icons.arrow_back,
-                                        color: Colors.black,
-                                        size: a.width / 15),
-                                  ),
-                                  onTap: () {
-                                    Navigator.pop(
-                                      context,
-                                    );
-                                  },
-                                ),
-                              ],
-                            ), 
-        ],),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(
+                  top: a.width / 15,
+                  right: a.width / 25,
+                  left: a.width / 25,
+                  bottom: a.width / 8.0),
+              child: Row(
+                children: <Widget>[
+                  InkWell(
+                    //back btn
+                    child: Container(
+                      width: a.width / 7,
+                      height: a.width / 10,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(a.width),
+                          color: Colors.white),
+                      child: Icon(Icons.arrow_back,
+                          color: Colors.black, size: a.width / 15),
+                    ),
+                    onTap: () {
+                      Navigator.pop(
+                        context,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: a.width,
+              alignment: Alignment.topLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "ผู้คนรอบตัว",
+                  style: TextStyle(color: Colors.white,fontSize: a.width/7),
+                ),
+                Text(
+                    "พบกับระบบค้นหาผู้คนรอบตัวคุณ \nแล้วปากระดาษใส่พวกเขาได้ทันที เร็วๆนี้",
+                    style: TextStyle(color: Colors.white,fontSize: a.width/20))
+              ],
+            )),
+          ],
+        ),
       ),
     );
   }
