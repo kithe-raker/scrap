6d6e0ff6f8054b773094b7e0cb9697205c07309f
Kitti_pop <jokkerlolyer20@gmail.com>
2020-04-16 13:59:39 +0700

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:scrap/provider/authen_provider.dart';
-import 'package:scrap/Page/createworld/ConfigWorld.dart';
-import 'package:scrap/services/auth.dart';
-import 'package:scrap/services/provider.dart';
 import 'package:scrap/Page/MainPage.dart';
-import 'package:scrap/Page/NewWorld.dart';
-import 'package:scrap/Page/createworld/ConfigWorld.dart';
-import 'package:scrap/Page/authentication/MainLogin.dart';
-import 'package:scrap/Page/authentication/registered/penname/PennameWithPassword.dart';
-import 'package:scrap/Page/authentication/not_registered/phone/PhoneWithOTP.dart';
-import 'package:scrap/Page/authentication/registered/phone/SetPenname.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
