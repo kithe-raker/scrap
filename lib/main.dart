a1b3bdcbb5691fcd4051ed19dfdc1c6ac54b3554
Kitti_pop <jokkerlolyer20@gmail.com>
2020-05-17 21:54:24 +0700

    add ads provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:scrap/Page/Gridfavorite.dart';
 import 'package:scrap/Page/MainPage.dart';
+import 'package:scrap/provider/AdsCounter.dart';
 import 'package:scrap/services/auth.dart';
-import 'package:scrap/services/provider.dart';
+import 'package:scrap/services/provider.dart' as prov;
 import 'package:firebase_admob/firebase_admob.dart';
 
 import 'Page/Auth.dart';
@@ -25,14 +27,20 @@ class MyApp extends StatelessWidget {
   //this is new branch
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      auth: Auth(),
-      child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Scrap.',
-          theme: ThemeData(
-              fontFamily: 'ThaiSans', unselectedWidgetColor: Colors.white),
-          home: MainPage()),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AdsCounterProvider>.value(
+            value: AdsCounterProvider())
+      ],
+      child: prov.Provider(
+        auth: Auth(),
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Scrap.',
+            theme: ThemeData(
+                fontFamily: 'ThaiSans', unselectedWidgetColor: Colors.white),
+            home: MainPage()),
+      ),
     );
   }
 }
