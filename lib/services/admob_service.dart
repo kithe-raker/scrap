b983f8e1ae5dbe09cd8c6172ac615bdf266999e1
Kitti_pop <jokkerlolyer20@gmail.com>
2020-06-05 14:43:51 +0700

    change ads id

--- lib/services/admob_service.dart
+++ lib/services/admob_service.dart
@@ -11,40 +11,7 @@ class AdmobService {
   }
 
   String getBannerAdId() {
-     if (Platform.isIOS){
-      return 'ca-app-pub-3940256099942544/6300978111';
-    } else if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
-    }
-    return null;
-  }
-
-  String getVideoAdId(){
-    if (Platform.isIOS){
-      return 'ca-app-pub-3940256099942544/8691691433';
-    } else if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/8691691433';
-    }
-    return null;
-  }
-}
-
-final ads = AdmobService();
-
-
-
-/*
-String getAdmobAppId() {
     if (Platform.isIOS) {
-      return 'ca-app-pub-3612265554509092~4730522974';
-    } else if (Platform.isAndroid) {
-      return 'ca-app-pub-3612265554509092~5449650222';
-    }
-    return null;
-  }
-
-  String getBannerAdId() {
-     if (Platform.isIOS){
       return 'ca-app-pub-3612265554509092/4043772045';
     } else if (Platform.isAndroid) {
       return 'ca-app-pub-3612265554509092/3753425177';
@@ -52,11 +19,24 @@ String getAdmobAppId() {
     return null;
   }
 
-  String getVideoAdId(){
-    if (Platform.isIOS){
-      return 'ca-app-pub-3612265554509092/8652228758';
+  String getVideoAdId() {
+    if (Platform.isIOS) {
+      return 'ca-app-pub-3612265554509092/6217554953';
     } else if (Platform.isAndroid) {
-      return 'ca-app-pub-3612265554509092/6245525449';
+      return 'ca-app-pub-3612265554509092/1348371653';
     }
     return null;
-  }*/
+  }
+}
+
+final ads = AdmobService();
+
+/*
+
+getbanner android => ca-app-pub-3612265554509092/3753425177
+getbanner ios => ca-app-pub-3612265554509092/4043772045
+getVedioAdid  android => ca-app-pub-3612265554509092/6245525449
+getVideoAdid ios => ca-app-pub-3612265554509092/8652228758
+*/
+
+
