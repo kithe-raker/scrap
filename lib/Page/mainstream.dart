d910a6852770ad0866ff35ea94afcc02295c5036
tah3168 <60140935+tah3168@users.noreply.github.com>
2020-06-14 17:35:49 +0700

    bottombar

--- lib/Page/mainstream.dart
+++ lib/Page/mainstream.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:scrap/Page/HomePage.dart';
+import 'package:scrap/widget/ScreenUtil.dart';
+import 'package:flutter_svg/svg.dart';
+
+var index = 0;
 
 class MainStream extends StatefulWidget {
   @override
@@ -9,6 +13,45 @@ class MainStream extends StatefulWidget {
 class _MainStreamState extends State<MainStream> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black, body: HomePage());
+    screenutilInit(context);
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: HomePage(),
+      bottomNavigationBar: bottom(),
+    );
+  }
+
+  Widget activebutton(var _index, String icon) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          index = _index;
+        });
+      },
+      child: Container(
+        height: screenWidthDp / 10,
+        width: screenWidthDp / 10,
+        child: SvgPicture.asset(icon,
+            color: index != _index ? Color(0xfff434343) : Colors.white),
+      ),
+    );
+  }
+
+  Widget bottom() {
+    return Container(
+      height: screenWidthDp / 5,
+      width: screenWidthDp,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          activebutton(0, 'assets/paper.svg'),
+          activebutton(1, 'assets/paper.svg'),
+          activebutton(2, 'assets/paper.svg'),
+          activebutton(3, 'assets/paper.svg'),
+          activebutton(4, 'assets/paper.svg'),
+        ],
+      ),
+    );
   }
 }
